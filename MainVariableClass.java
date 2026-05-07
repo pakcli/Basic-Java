568dc388d1f078f62dd2c0aa17e0803a43f71df8
fsl_eightvram\fadhlisl <dcalibri@gmail.com>
2026-05-07 15:01:49 +0700

    day 1

---
+++ MainVariableClass.java
@@ -0,0 +1,8 @@
+public class MainVariableClass {
+    public static void main(String[] args) {
+        // 1. Buatlah sebuah deklarasi variabel dengan tipe data float
+        //    yang bernama 'Strukturbaris'
+        float StrukturBaris = 9.25f;
+        System.out.println("struktur baris:" + StrukturBaris);
+    }
+}
